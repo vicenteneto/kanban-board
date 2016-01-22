e17febd6c6d4c32a5ca73ce75201be3a6f19139f
Vicente Silva <vicente.silva@embedded.ufcg.edu.br>
2016-01-21 23:28:07 -0300

    Saving projects reference on project creation

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -1,30 +1,31 @@
 module.exports = function (app) {
-    var User = app.models.user;
     var Project = app.models.project;
+    var User = app.models.user;
 
     return {
         create: function (req, res) {
             Project.create(req.body.project, function (err, project) {
-                User.findById(req.session.user._id, function (err, user) {
-                    user.projects.push(project._id);
+                var message;
 
-                    user.save(function (err) {
-                        var message = 'Project created successfully!';
+                if (err) {
+                    req.session.hasError = err;
+                    message = 'Error creating project!';
 
-                        if (err) {
-                            message = 'Error creating project!';
-                        }
+                    res.redirect('/home');
+                } else {
+                    User.findById(req.session.user._id, function (err, user) {
+                        user.projects.push(project._id);
+                        user.save();
+
+                        message = 'Project created successfully!';
 
                         req.session.hasError = err;
                         req.session.message = message;
 
                         res.redirect('/home');
                     });
-                });
+                }
             });
-        },
-        list: function (req, res) {
-
         }
     };
 };
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
     var loginController = app.controllers.login;
 
+    app.get('/login', loginController.index);
     app.post('/login', loginController.login);
     app.get('/logout', loginController.logout);
 };
