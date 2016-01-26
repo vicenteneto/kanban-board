87c500fb9197c86747cc361eee55ab60fcf4c1c7
Vicente Silva <vicente.silva@embedded.ufcg.edu.br>
2016-01-25 22:26:55 -0300

    Implemented project edit and remove functions

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,9 @@
 $(function () {
     window.setTimeout(function () {
-        $(".alert-dismissible").fadeOut("slow");
+        $('.alert-dismissible').fadeOut('slow');
     }, 5000);
 
     $('input:text:visible:first').focus();
-
-    $('#signUpModal').on('shown.bs.modal', function () {
-        $('#nameField').focus()
-    });
 
     $('#loginForm').validate();
     $('#signInForm').validate();
